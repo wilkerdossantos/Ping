c49d497e077537626d12b87ed9fa8e91f34d9f7e
Wilker Santos <78004450+wilkerdossantos@users.noreply.github.com>
2021-03-20 02:05:08 -0300

    Add files via upload

---
+++ Ping.py
@@ -0,0 +1,34 @@
+import subprocess
+
+class Ping:
+    arquivo = open('pingou.txt','w')
+    arquivo.close()
+
+    def __init__(self):
+        self.lista_ip = self.import_ip()
+
+    def pinga(self):
+        for ip in self.lista_ip:
+            ping = subprocess.run(['ping','-w','1',f'{ip}'], capture_output=True)
+            print(ip, self.valida_ping(ping, ip))
+
+    def valida_ping(self, ping, ip):
+        saida = str(ping.stdout)
+        indice = saida.find(',') + 2
+        if saida[indice] != '0':
+            arquivo = open('pingou.txt','a')
+            arquivo.write(f'{ip}\n')
+            arquivo.close()
+            return True
+        else:
+            return False
+
+    def import_ip(self):
+        arquivo = open('ip.txt', 'r')
+        lista = [linha.strip() for linha in arquivo]
+        arquivo.close()
+        return lista
+
+ip = Ping()
+
+ip.pinga()
